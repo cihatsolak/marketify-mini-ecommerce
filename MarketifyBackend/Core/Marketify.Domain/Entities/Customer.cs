105f8e507be5644aed6f0bc7a5f5d9e1f6d83855
Cihat Solak <cihatsolak@hotmail.com>
2023-03-16 22:28:54 +0300

    feat(backend): add database entities

--- MarketifyBackend/Core/Marketify.Domain/Entities/Customer.cs
+++ MarketifyBackend/Core/Marketify.Domain/Entities/Customer.cs
@@ -1,10 +1,8 @@
 ﻿namespace Marketify.Domain.Entities
 {
-    public class Product : BaseEntity
+    public class Customer : BaseEntity
     {
         public string Name { get; set; }
-        public int Stock { get; set; }
-        public long Price { get; set; }
 
         public ICollection<Order> Orders { get; set; }
     }
---
+++ MarketifyBackend/Core/Marketify.Domain/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Marketify.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string Description { get; set; }
+        public string Address { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
