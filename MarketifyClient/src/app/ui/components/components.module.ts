7bc9b32bbba1153606746283e0815d3d8efc2f89
Cihat Solak <cihatsolak@hotmail.com>
2023-03-17 22:58:09 +0300

    feat(client): add home component for ui

--- MarketifyClient/src/app/ui/components/components.module.ts
+++ MarketifyClient/src/app/ui/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasketsModule } from './baskets/baskets.module';
+import { HomeModule } from './home/home.module';
+import { ProductsModule } from './products/products.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BasketsModule,
+    HomeModule,
+    ProductsModule
   ]
 })
 export class ComponentsModule { }
