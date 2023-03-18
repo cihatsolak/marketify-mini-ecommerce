09cf974aa33b7738bd982076c666dbfe38525a3c
Cihat Solak <cihatsolak@hotmail.com>
2023-03-18 17:27:04 +0300

    feat(client): add spinner library

--- MarketifyClient/src/app/admin/components/customers/customers.component.ts
+++ MarketifyClient/src/app/admin/components/customers/customers.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss']
 })
-export class CustomersComponent {
+export class CustomersComponent extends BaseComponent implements OnInit  {
+  /**
+   *
+   */
+  constructor(spinner: NgxSpinnerService) {
+    super(spinner);
+  }
+
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallAtom);
+  }
 
 }
--- MarketifyClient/src/app/app.module.ts
+++ MarketifyClient/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 @NgModule({
   declarations: [
     AppComponent
@@ -18,7 +18,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     AdminModule,
     UiModule,
-    ToastrModule.forRoot()  
+    ToastrModule.forRoot(),
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
