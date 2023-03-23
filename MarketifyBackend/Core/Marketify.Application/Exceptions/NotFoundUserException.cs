083787d1786d55da773d48f0536d7a735c902a12
Cihat Solak <cihatsolak@hotmail.com>
2023-03-23 20:48:28 +0300

    feat(backend): add custom error classes

---
+++ MarketifyBackend/Core/Marketify.Application/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,17 @@
+﻿namespace Marketify.Application.Exceptions
+{
+    public class NotFoundUserException : Exception
+    {
+        public NotFoundUserException() : base("Username or password is wrong.")
+        {
+        }
+
+        public NotFoundUserException(string message) : base(message)
+        {
+        }
+
+        public NotFoundUserException(string message, Exception exception) : base(message, exception)
+        {
+        }
+    }
+}
---
+++ MarketifyBackend/Core/Marketify.Application/Exceptions/UserCreateFailedException.cs
@@ -0,0 +1,17 @@
+﻿namespace Marketify.Application.Exceptions
+{
+    public class UserCreateFailedException : Exception
+    {
+        public UserCreateFailedException() : base("An unexpected error was encountered while creating a user!")
+        {
+        }
+
+        public UserCreateFailedException(string message) : base(message)
+        {
+        }
+
+        public UserCreateFailedException(string message, Exception exception) : base(message, exception)
+        {
+        }
+    }
+}
